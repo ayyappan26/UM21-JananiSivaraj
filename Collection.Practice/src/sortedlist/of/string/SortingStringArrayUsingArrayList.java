a97e5e2a64d92c5d82925dd8a46bb3ee48df7abe
JSivaraj <JSivaraj@USI-4051L.ultramain.com>
2021-09-02 11:21:43 +0530

    Collection practice

---
+++ Collection.Practice/src/sortedlist/of/string/SortingStringArrayUsingArrayList.java
@@ -0,0 +1,33 @@
+package sortedlist.of.string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingStringArrayUsingArrayList {
+
+	public static void main(String[] args) {
+
+		/*
+		 * creating string array of elements
+		 */
+		String string_array[] = { "orange", "apple", "123", "*&^", "zebra" };
+		/*
+		 * converting string array to list
+		 */
+		List<String> list1 = Arrays.asList(string_array);
+		/*
+		 * printing the list of sting array
+		 */
+		System.out.println("before sorting");
+		System.out.println(list1);
+		/*
+		 * sorting the list using Collections.sort(list)
+		 */
+		Collections.sort(list1);
+		System.out.println("after sorting");
+		System.out.println(list1);
+	}
+
+}
