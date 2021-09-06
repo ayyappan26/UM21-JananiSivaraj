83ff5ab04b28447c490cfe5c71866ffdaef483c7
JSivaraj <JSivaraj@USI-4051L.ultramain.com>
2021-09-06 19:07:10 +0530

    interacting SQL using HashMap

--- ultramain.sql.hashmap.project/src/main/MainClass.java
+++ ultramain.sql.hashmap.project/src/main/MainClass.java
@@ -1,7 +1,13 @@
 package main;
 
 import business.object.BusinessObject;
-
+ /**
+ * @author JSivaraj
+ * From main method I m calling BO Class view Employee details class. Where
+ * 1. it will ask users to pick choice.
+ * 2. and according to the choice it will map the method to be executed.
+ *
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
